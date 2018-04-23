9f94fbd3c7d8f2f63afc71f2d2bf34fd7a0857ff
kisskaishen <kisskaishen@163.com>
2018-04-23 09:47:25 +0800

    添加活动入口

--- src/components/index/indexList.vue
+++ src/components/index/indexList.vue
@@ -4,6 +4,9 @@
          infinite-scroll-distance="100">
         <index-banner :banner="bannerData"></index-banner>
         <menu-list :info="menuData"></menu-list>
+        <router-link to="">
+            <img src="" alt="">
+        </router-link>
         <goods-list :list="listData"></goods-list>
     </div>
 </template>
@@ -21,6 +24,7 @@
                 listData: [],            // 首页商品列表
                 page: 0,
                 loading: false,
+                enterInfo:{},
             }
         },
         components: {IndexBanner, MenuList, GoodsList},
@@ -50,6 +54,19 @@
                     .catch((err) => {
                         console.log(err)
                     })
+            },
+            getEnterInfo() {
+                this.$post('ad/positions',{})
+                    .then((res)=>{
+                        if (res.status == '1') {
+                            this.enterInfo = res.result
+                        } else {
+                            alert('出错了')
+                        }
+                    })
+                    .catch(err=>{
+                        console.log(err)
+                })
             },
             // 加载更多
             loadMore() {
